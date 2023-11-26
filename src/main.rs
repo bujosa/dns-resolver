266b5e3a7c9913752ea89e0162e420595153ac69
bujosa <davidbujosa@gmail.com>
2023-11-26 18:18:51 -0400

    fix(main): update issue with localhost

--- src/main.rs
+++ src/main.rs
@@ -32,5 +32,5 @@ async fn main() {
         .and(resolver.clone())
         .and_then(handle_resolve);
 
-    warp::serve(resolve_route).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(resolve_route).run(([0, 0, 0, 0], 3030)).await;
 }
